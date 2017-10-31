60d81799da0db14e74f3352abce091f4d3c54adc
KancerEzeroglu <ezeroglukancer@gmail.com>
2017-10-31 16:48:33 +0300

    forum continue

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer} from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-    posts_reducer: PostsReducer
-
+    posts_reducer: PostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
